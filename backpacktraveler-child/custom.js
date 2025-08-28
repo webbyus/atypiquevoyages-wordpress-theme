12ee2a99c25f6ad342f132f0f9c48c5cc19a7a76
Sameera <43690533+webbyus@users.noreply.github.com>
2025-08-28 10:26:12 +0530

    Remove unused functions and add reservation form with datepicker enhancements

--- backpacktraveler-child/custom.js
+++ backpacktraveler-child/custom.js
@@ -1,8 +1,33 @@
-jQuery(document).ready(function ($) {
-
+jQuery(function ($) {
   let dateToday = new Date();
-  $(".past-days-disabled-date-field").datepicker({
-    dateFormat: 'dd-mm-yy',
+
+  // apply day picker only to other fields (not #date-start)
+  $(".past-days-disabled-date-field").not("#date-start").datepicker({
+    dateFormat: "dd-mm-yy",
     minDate: dateToday,
+  });
+
+  // month–year only for #date-start
+  $("#date-start").datepicker({
+    changeMonth: true,
+    changeYear: true,
+    showButtonPanel: true,
+    dateFormat: "MM yy",
+    onClose: function (dateText, inst) {
+      $(this).datepicker(
+        "setDate",
+        new Date(inst.selectedYear, inst.selectedMonth, 1)
+      );
+    },
+    beforeShow: function () {
+      setTimeout(function () {
+        $(".ui-datepicker-calendar").hide();
+      }, 0);
+    },
+  });
+
+  // keep calendar hidden when focusing
+  $("#date-start").focus(function () {
+    $(".ui-datepicker-calendar").hide();
   });
 });
